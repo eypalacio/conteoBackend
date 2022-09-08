407f589db42584198412ebe44ab84aca082fdbc7
elaine <yane4112@gmail.com>
2022-09-08 14:09:28 -0400

    first commit

--- urls_api/url.js
+++ urls_api/url.js
@@ -4,29 +4,11 @@
 var express = require('express');
 
 /** controlador de la api */
-var swiftController = require('../controllers/swift');
-var repairController = require('../controllers/repair');
-var slbtrController = require('../controllers/slbtr');
-var execFileController = require('../controllers/execFileETL');
-var mtSlbtrController = require('../controllers/mtSLBTR');
+var busquedaUsuariosController = require('../controllers/busquedaUsuarios');
+var busquedasRecientesController = require('../controllers/busquedasRecientes');
 
 /** router */
 var api = express.Router();
-
-api.get('/mensajesT24',swiftController.mensajesT24);
-api.get('/buscarMensajes',swiftController.buscarMensaje);
-api.get('/mostrarHoras',swiftController.mostrarHoras);  
-api.get('/ejecutarConteoT24',execFileController.ejecutarConteoT24);
-
-api.get('/mensajesSLBTR',slbtrController.mensajesSLBTR); 
-api.get('/conteoSLBTR',slbtrController.conteoSLBTR);
-api.get('/ejecutarConteoSlbtr',execFileController.ejecutarConteoSLBTR)
-
-api.get('/repair',repairController.repair);
-api.put('/setEstado',repairController.setEstado);
-
-api.get('/mt299',mtSlbtrController.getMT299);
-api.get('/mt206slbtr',mtSlbtrController.getMT206SLBTR);
 
 
 module.exports = api;
